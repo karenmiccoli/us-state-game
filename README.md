2a8eb5a2b5b81723ec56f10161fe82256a209986
karenmiccoli <karenjeet.chahal@gmail.com>
2024-01-09 13:56:32 +0000

    Add logic for US state guessing game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # us-state-game
+
+## Write a game to guess all 50 US states
+
+## How to play
+Enter the name of a state into the textbox.\
+Try to answer all 50!
---
+++ main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas
+from turtle import Screen
+
+EXIT_COMMAND = "Exit"
+FONT = ("Courier", 12, "bold")
+
+screen = Screen()
+screen.title("US States Game")
+image = "blank_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+correct_guesses = []
+
+while len(correct_guesses) < 50:
+    user_guess = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="What's another states "
+                                                                                            "name?").title()
+
+    if user_guess == EXIT_COMMAND:
+        break
+    if user_guess in states:
+        correct_guesses.append(user_guess)
+
+        state_name = turtle.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        state_info = data[data.state == user_guess]
+        state_name.goto(int(state_info.x.iloc[0]), int(state_info.y.iloc[0]))
+        state_name.color("red")
+        state_name.write(state_info.state.item(), font=FONT)
+
+missed_states = [x for x in states if x not in set(correct_guesses)]
+df = pandas.DataFrame(missed_states, columns=['missed_states'])
+df.to_csv("results.csv")
+
